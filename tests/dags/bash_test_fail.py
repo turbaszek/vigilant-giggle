ae63ab5720f8bfbb65ac8945f027b06a1deb7627
Tomasz Urbaszek <tomasz.urbaszek@polidea.com>
2020-12-01 11:55:38 +0100

    Add fixed date

--- tests/dags/bash_test_fail.py
+++ tests/dags/bash_test_fail.py
@@ -1,6 +1,7 @@
 import datetime
 from airflow.models import DAG
 from airflow.operators.bash_operator import BashOperator
+from airflow.utils.dates import days_ago
 
 default_args = {'wait_for_downstream': True, 'provide_context': False}
 
@@ -15,23 +16,58 @@ with DAG(
         bash_cmd_j001 = BashOperator(
             task_id='bash_cmd_j001_' + str(target_date),
             bash_command='echo "bash_cmd_001"',
-            trigger_rule='all_success', dag=dag)
+            trigger_rule='all_success')
         bash_cmd_j002 = BashOperator(
             task_id='bash_cmd_j002_' + str(target_date),
             bash_command='echo "bash_cmd_002"',
-            trigger_rule='all_success', dag=dag)
+            trigger_rule='all_success')
         bash_cmd_j003 = BashOperator(
             task_id='bash_cmd_j003_' + str(target_date),
             bash_command='echo "bash_cmd_003"',
-            trigger_rule='all_success', dag=dag)
+            trigger_rule='all_success')
         bash_cmd_j004 = BashOperator(
             task_id='bash_cmd_j004_' + str(target_date),
             bash_command='echo "bash_cmd_004"',
-            trigger_rule='all_success', dag=dag)
+            trigger_rule='all_success')
         bash_cmd_j005 = BashOperator(
             task_id='bash_cmd_j005_' + str(target_date),
             bash_command='echo "bash_cmd_005"',
-            trigger_rule='all_success', dag=dag)
+            trigger_rule='all_success')
+        if (target_date % 2) == 0:
+            bash_cmd_j001 >> bash_cmd_j002 >> bash_cmd_j003 >> bash_cmd_j004 >> bash_cmd_j005
+        else:
+            bash_cmd_j001 >> bash_cmd_j005 >> bash_cmd_j004 >> bash_cmd_j003 >> bash_cmd_j002
+
+
+
+with DAG(
+    "bash_test_fail_shrubbery_2",
+    start_date=days_ago(1),
+    schedule_interval='*/2 * * * *',
+    default_args=default_args,
+    max_active_runs=1
+) as dag2:
+    for target_date in range(0, 10):
+        bash_cmd_j001 = BashOperator(
+            task_id='bash_cmd_j001_' + str(target_date),
+            bash_command='echo "bash_cmd_001"',
+            trigger_rule='all_success')
+        bash_cmd_j002 = BashOperator(
+            task_id='bash_cmd_j002_' + str(target_date),
+            bash_command='echo "bash_cmd_002"',
+            trigger_rule='all_success')
+        bash_cmd_j003 = BashOperator(
+            task_id='bash_cmd_j003_' + str(target_date),
+            bash_command='echo "bash_cmd_003"',
+            trigger_rule='all_success')
+        bash_cmd_j004 = BashOperator(
+            task_id='bash_cmd_j004_' + str(target_date),
+            bash_command='echo "bash_cmd_004"',
+            trigger_rule='all_success')
+        bash_cmd_j005 = BashOperator(
+            task_id='bash_cmd_j005_' + str(target_date),
+            bash_command='echo "bash_cmd_005"',
+            trigger_rule='all_success')
         if (target_date % 2) == 0:
             bash_cmd_j001 >> bash_cmd_j002 >> bash_cmd_j003 >> bash_cmd_j004 >> bash_cmd_j005
         else:
