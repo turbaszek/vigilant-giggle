1c290d17faddd6ed9b7b921a89136246c5b3d070
Tomasz Urbaszek <tomasz.urbaszek@polidea.com>
2020-12-10 13:28:40 +0100

    Add always failing sensor

--- tests/dags/old_issue.py
+++ tests/dags/old_issue.py
@@ -1,6 +1,10 @@
-from airflow import models
 from airflow.operators.dummy_operator import DummyOperator
 from datetime import datetime, timedelta
+from time import sleep
+
+from airflow import DAG
+from airflow.sensors.base_sensor_operator import BaseSensorOperator
+from airflow.utils.dates import days_ago
 
 from airflow.sensors.external_task_sensor import ExternalTaskSensor
 
@@ -12,7 +16,7 @@ default_args = {
 }
 dag_name = 'master_dag'
 
-with models.DAG(
+with DAG(
     dag_name,
     default_args=default_args,
     schedule_interval='*/3 * * * *',
@@ -25,7 +29,7 @@ with models.DAG(
         task_id=f'start'
     )
 
-with models.DAG(
+with DAG(
     'target_dag',
     default_args=default_args,
     schedule_interval='*/3 * * * *',
@@ -38,6 +42,29 @@ with models.DAG(
         task_id='wait-task',
         external_dag_id=dag_name,
         external_task_id='start',
+        poke_interval=60 * 5,
+        mode='reschedule'
+    )
+
+
+class DummySensor(BaseSensorOperator):
+    def __init__(self, t=0, **kwargs):
+        self.t = t
+        super().__init__(**kwargs)
+
+    def poke(self, context):
+        sleep(self.t)
+        return False
+
+
+with DAG(
+    "other_dag",
+    start_date=days_ago(1),
+    schedule_interval="*/2 * * * *",
+    catchup=False
+) as dag3:
+    DummySensor(
+        task_id='wait-task',
         poke_interval=60 * 5,
         mode='reschedule'
     )
